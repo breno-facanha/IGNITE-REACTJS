f03ab0bff1f04e9a276bb13e8e03ad7bb8ebd722
breno.facanha <brenofacanha.ti@gmail.com>
2023-01-30 11:26:33 -0300

    Estilizando o componente sidebar

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,26 +1,31 @@
 import { Header } from "./components/Header"
 import { Post } from "./Post"
+import { Sidebar } from "./components/Sidebar"
+
+import styles from './App.module.css'
+
 import './global.css'
+
 
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post 
-        author="Breno Façanha" 
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, pariatur expedita. Magni eaque facilis numquam, libero quam accusamus? Animi veniam veritatis vero repellat officia quidem voluptatum ea praesentium temporibus aliquam."
-      />
-      <Post 
-       author="Samara Vasconcelos" 
-       content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, pariatur expedita. Magni eaque facilis numquam, libero quam accusamus? Animi veniam veritatis vero repellat officia quidem voluptatum ea praesentium temporibus aliquam."
-      />
-      <Post />
-      <Post />
-      
-    </div>
-    
-   
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Breno Façanha" 
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, pariatur expedita. Magni eaque facilis numquam, libero quam accusamus? Animi veniam veritatis vero repellat officia quidem voluptatum ea praesentium temporibus aliquam."
+          />
+          <Post 
+            author="Samara Vasconcelos" 
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, pariatur expedita. Magni eaque facilis numquam, libero quam accusamus? Animi veniam veritatis vero repellat officia quidem voluptatum ea praesentium temporibus aliquam."
+          />
+        </main>
+      </div>
+    </div> 
   )
 }
 
